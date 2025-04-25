3377eec34c6274b9266ad336b89e5cbba1c111b3
programmarimran <programmarimran@gmail.com>
2025-04-26 00:25:46 +0600

    addd a semp

--- src/Components/Doctors/Doctors.jsx
+++ src/Components/Doctors/Doctors.jsx
@@ -21,7 +21,7 @@ const Doctors = () => {
 //  console.log(doctors)
   return (
     <div className="py-20">
-        <h1 className=" text-4xl font-extrabold text-center">Our Best Doctors</h1>
+        <h1 className=" text-4xl font-extrabold text-center">Our i Best Doctors</h1>
         <p className="text-base font-normal text-center w-4/5 mx-auto mt2">Our platform connects you with verified, experienced doctors across various specialties — all at your convenience. Whether it's a routine checkup or urgent consultation, book appointments in minutes and receive quality care you can trust.</p>
         
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3 mx-auto">
